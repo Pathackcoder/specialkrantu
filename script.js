a101c37615b543eddc5c254ebf602f5f2baf7755
Amit Pathak <91179150+Pathackcoder@users.noreply.github.com>
2024-04-25 00:49:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('showButton').addEventListener('click', function() {
+    var message = document.getElementById('message');
+    if (message.style.display === 'none') {
+        message.style.display = 'block';
+    } else {
+        message.style.display = 'none';
+    }
+});
